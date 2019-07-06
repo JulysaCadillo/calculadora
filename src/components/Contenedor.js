63ca5eb15c08a2a6ecd0e8659748d38964d41fc0
julysa <jmcadillo.06@gmail.com>
2019-07-06 14:22:25 -0500

    agregando

--- src/components/Contenedor.js
+++ src/components/Contenedor.js
@@ -42,28 +42,61 @@ constructor(){
                
                 this.setState({resultado:resultado +'6'})
             }
+            clickpordos=()=>{
+                const {resultado} =this.state;
+              const nwresultado =(parseInt(resultado)*2)+"";
+                this.setState({resultado:nwresultado })
+            }
+           
+            clickigual=()=>{
+                const {resultado} =this.state;
+                const {base} =this.state;
+                const {alv} =this.state;
+                
+                if (alv==='suma') {
+                     const suma = (parseInt(base) +parseInt(resultado)+"")
+               this.setState({resultado:suma})
+                } else if(  alv==='resta') {
+                    const suma = (parseInt(base) -parseInt(resultado)+"")
+               this.setState({resultado:suma})
+                }else if(  alv==='division') {
+                    const suma = (parseInt(base) /parseInt(resultado)+"")
+               this.setState({resultado:suma})
+                }else if(  alv==='multiplicar') {
+                    const suma = (parseInt(base) *parseInt(resultado)+"")
+               this.setState({resultado:suma})
+                }
+               
+            }
             clickmas=()=>{
                 const {resultado} =this.state;
               
                 this.setState({resultado:''})
                 this.setState({base:resultado})
-                
+                this.setState({alv:'suma'})
 
                 
             }
-            clickigual=()=>{
+            clickresta=()=>{
                 const {resultado} =this.state;
-                const {base} =this.state;
-                const suma = (parseInt(base) +parseInt(resultado)+"")
-               this.setState({resultado:suma})
+               
+                this.setState({resultado:''})
+                this.setState({base:resultado})
+                this.setState({alv:'resta'})
             }
-           
-
-
-            clickpordos=()=>{
+            clickdivision=()=>{
                 const {resultado} =this.state;
-              const nwresultado =(parseInt(resultado)*2)+"";
-                this.setState({resultado:nwresultado })
+               
+                this.setState({resultado:''})
+                this.setState({base:resultado})
+                this.setState({alv:'division'})
+            }
+            clickmultiplo=()=>{
+                const {resultado} =this.state;
+               
+                this.setState({resultado:''})
+                this.setState({base:resultado})
+                this.setState({alv:'multiplicar'})
             }
     render(){
 
@@ -79,20 +112,24 @@ constructor(){
             <button onClick={this.clickuno}  >1 </button>
             <button onClick={this.clickdos}  >2 </button>
             <button onClick={this.clicktres}  >3 </button>
+            <button onClick={this.clickmultiplo}  >*</button>
        
              </div>
              <div >
            <button onClick={this.clickcuatro}  >4 </button>
             <button onClick={this.clickcinco}  >5 </button>
             <button onClick={this.clickseis}  >6 </button>
+            <button onClick={this.clickdivision}  >/</button>
        
              </div>
+             
 
             
-         
+            <button onClick={this.clickp}  >0</button>
             <button onClick={this.clickigual}  >= </button>
             <button onClick={this.clickmas}  >+</button>
-            <button onClick={this.clickp}  >.</button>
+            <button onClick={this.clickresta}  >-</button>
+            
             </div>
         )
         }
